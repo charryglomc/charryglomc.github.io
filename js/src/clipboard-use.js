bd415d1e4451780f0eeedf5300c798d5360028ad
charryglomc <798577670@qq.com>
2020-04-07 15:18:24 +0800

    Site updated: 2020-04-07 15:18:22

---
+++ js/src/clipboard-use.js
@@ -0,0 +1,17 @@
+/*ҳ��������ɺ󣬴������ư�ť*/
+!function (e, t, a) { 
+  /* code */
+  var initCopyCode = function(){
+    var copyHtml = '';
+    copyHtml += '<button class="btn-copy" data-clipboard-snippet="">';
+    copyHtml += '  <i class="fa fa-globe"></i><span>copy</span>';
+    copyHtml += '</button>';
+    $(".highlight .code pre").before(copyHtml);
+    new ClipboardJS('.btn-copy', {
+        target: function(trigger) {
+            return trigger.nextElementSibling;
+        }
+    });
+  }
+  initCopyCode();
+}(window, document);
