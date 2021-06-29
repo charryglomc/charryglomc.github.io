ba3eb9a0230f2a8048e02ae2fae452cf39736228
charryglomc <798577670@qq.com>
2021-06-29 14:06:19 +0800

    Site updated: 2021-06-29 14:05:54

--- dist/music.js
+++ dist/music.js
@@ -1,3 +1,3 @@
-// build time:Tue Jun 29 2021 10:36:26 GMT+0800 (GMT+08:00)
+// build time:Tue Jun 29 2021 14:05:18 GMT+0800 (GMT+08:00)
 const ap=new APlayer({container:document.getElementById("aplayer"),fixed:true,autoplay:false,listMaxHeight:60,audio:[{name:"五月雨",artist:"高梨康治",url:"http://m10.music.126.net/20200408211232/d55f8c982004dc98b4b7bf7627a4259e/ymusic/d741/ca68/da8c/f6110877fc505a5c4c9a2a1de92a9dc1.mp3",cover:"https://p1.music.126.net/2s6Sxg_TE20FnQsCWw80JQ==/109951163338274299.jpg"},{name:"落叶船",artist:"高梨康治",url:"http://m10.music.126.net/20200408211610/da2a0414e3326d40feb98a3963b0a82b/ymusic/cff5/46be/856f/ed91aee0e35992d497d526c10048d730.mp3",cover:"https://p1.music.126.net/2s6Sxg_TE20FnQsCWw80JQ==/109951163338274299.jpg"}]});
 //rebuild by neat 
