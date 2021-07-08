06e9624858b9a9289f56540ba9ed8cab9f0a7e8b
charryglomc <798577670@qq.com>
2021-07-08 15:46:45 +0800

    Site updated: 2021-07-08 15:46:16

--- js/src/scroll-cookie.js
+++ js/src/scroll-cookie.js
@@ -1,3 +1,3 @@
-// build time:Fri Jul 02 2021 09:30:54 GMT+0800 (GMT+08:00)
+// build time:Thu Jul 08 2021 15:44:12 GMT+0800 (GMT+08:00)
 $(document).ready(function(){var o=window.location.href.replace(window.location.origin,"");var i;$(window).on("scroll",function(){clearTimeout(i);i=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+o,{expires:365,path:""})},250)});if(Cookies.get("scroll-cookie")!==undefined){var e=Cookies.get("scroll-cookie").split("|");if(e[1]==o){$(window).scrollTop(e[0])}}});
 //rebuild by neat 
